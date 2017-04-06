1ba4755751e314a494021226cf25aa3d15ffc070
Chris <chris@cypheros.co>
2017-04-06 13:21:24 -0400

    Merge tag 'android-7.1.2_r5' into nougat-mr1
    Android 7.1.2 Release 5
    
    Change-Id: Ice7765072a46c518fe9bd4b7c53656eef1c720de

--- btif/include/btif_storage.h
+++ btif/include/btif_storage.h
@@ -288,16 +288,14 @@ BOOLEAN btif_storage_is_restricted_device(const bt_bdaddr_t *remote_bd_addr);
 
 /*******************************************************************************
 **
-** Function         btif_storage_is_wiimote
+** Function         btif_storage_get_num_bonded_devices
 **
-** Description      BTIF storage API - checks if this device is a wiimote
+** Description      BTIF storage API - Gets the number of bonded devices
 **
-** Returns          TRUE   if the device is found in wiimote device list
-**                  FALSE otherwise
+** Returns          the number of bonded devices
 **
 *******************************************************************************/
-BOOLEAN btif_storage_is_wiimote(bt_bdaddr_t *remote_bd_addr, bt_bdname_t *remote_bd_name);
-
+int btif_storage_get_num_bonded_devices(void);
 #if (BLE_INCLUDED == TRUE)
 bt_status_t btif_storage_add_ble_bonding_key( bt_bdaddr_t *remote_bd_addr,
                                               char *key,
